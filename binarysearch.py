a36dd2d11ccf72b25d429642dc05d2944038f193
Mwatim <timmwatela@gmail.com>
2024-01-04 08:59:48 +0300

    Code to do Binary Search in Python

---
+++ binarysearch.py
@@ -0,0 +1,39 @@
+def binarySearch(list_of_numbers, target):
+    """
+    This function takes in a list of numbers and returns the target.
+
+    Parameters:
+    -List of numbers
+    -Target
+
+    Results:
+    -Target index if it exists
+    """
+    (left, right) = (0, len(list_of_numbers) - 1)
+
+    while left <= right:
+        # mitigate integer overflow
+        mid_value = (right - left) // 2
+        if target == list_of_numbers[mid_value]:
+            return mid_value
+        
+        elif target < list_of_numbers[mid_value]:
+            return mid_value - 1
+        
+        else:
+            left = mid_value + 1
+
+    return -1
+
+if __name__ == '__main__':
+    list_of_numbers = [2,3,5,81,95,52,42,7,8,10,12,15,18,20]
+    target = 52
+
+    list_of_numbers = sorted(list_of_numbers)
+
+    index = binarySearch(list_of_numbers, target)
+
+    if index != -1:
+        print(f"Element found at index {index}")
+    else:
+        print("Element not found in the list")
